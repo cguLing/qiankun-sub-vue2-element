3ffbc8a7e72ad8877dacf38bc8895908407b9e2f
unknown <taoyl>
2022-03-15 16:14:00 +0800

    update:路由可动态

--- src/router/index.js
+++ src/router/index.js
@@ -1,109 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '@/components/main'
-
+import store from '@/store'
+import routesDefault from './routes'
 Vue.use(VueRouter)
+/**
+ * 后台查询的菜单数据拼装成路由格式的数据
+ * @param routes
+ */
+ function generaRoute(routes, data) {
+  data.forEach((item) => {
+    const menu = {
+      path: item.path,
+      component:
+        item.component === "Main" ? Main : ()=>import(`@/view${item.component}`), 
+      children: [],
+      name: item.name,
+      meta: {
+        title: item.meta.title,
+        icon: item.meta.icon,
+        layout: item.meta.layout,
+        hideInMenu: item.meta.hideInMenu,
+        right: item.meta.right
+      },
+    };
+    if (item.children) {
+      generaRoute(menu.children, item.children);
+    }
+    routes.push(menu);
+  });
+}
 
-const routes =  [
-  {
-    path: '/',
-    name: '/',
-    redirect: '/home',
-    meta: {
-      layout: true,
-    },
-    component: Main,
-    children: [
-      {
-        path: '/home',
-        name: 'home',
-        meta: {
-          hideInMenu: true,
-          layout: true,
-          title: '首页'
-        },
-      },
-      {
-        path: '/dept',
-        name: 'dept',
-        meta: {
-          hideInMenu: false,
-          icon: 'logo-windows',
-          title: '业务部门负责人'
-        },
-        component: () => import('@/view/dept/index.vue')
-      }
-    ]
-  },
-  {
-    path: '/project',
-    meta: {
-      layout: true,
-    },
-    component: Main,
-    children: [
-      {
-        path: '/project',
-        name: 'project',
-        meta: {
-          icon: 'ios-navigate',
-          title: '项目组'
-        },
-        component: () => import('@/view/project/index.vue')
-      }
-    ]
-  },
-  {
-    path: '/myConnect',
-    name: 'myConnect',
-    meta: {
-      icon: 'ios-bookmarks',
-      title: '我的服务'
-    },
-    component: Main,
-    children: [
-      {
-        path: '/myConnect/IDC',
-        name: 'myConnect_IDC',
-        meta: {
-          title: '服务1'
-        },
-        component: () => import('@/view/service/index.vue')
-      },
-      {
-        path: '/myConnect/IT',
-        name: 'myConnect_IT',
-        meta: {
-          title: '服务2'
-        },
-        component: () => import('@/view/service/index.vue')
-      }
-    ]
-  },
-  {
-    path: 'https://www.baidu.com/',
-    name: 'doc',
-    meta: {
-      icon: 'ios-navigate',
-      title: '操作文档',
-      target: 'link'
-    }
-  },
-  {
-    path: '/404',
-    meta: {
-      layout: true,
-    },
-    component: () => import('@/view/error/404/index.vue')
-  },
-  {
-    path: '*',
-    name: '*',
-    redirect: '/404',
-    meta: {
-      layout: true,
-    }
+let result = []
+generaRoute(result,routesDefault)
+let user = store.state.user.username
+let right = store.state.user.right
+const routes = result.filter((obj)=>{
+  if(!obj.meta.right) return true
+  for (let i = 0; i < obj.meta.right.length; i++) {
+    const item = obj.meta.right[i];
+    if(right[item].includes(user)) return true
   }
-]
-
+  return false
+})
 export default routes
