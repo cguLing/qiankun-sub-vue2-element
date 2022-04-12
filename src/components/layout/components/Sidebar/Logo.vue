b5adca5c5929e6d029e0697afebbbb9979bb081d
unknown <taoyl>
2022-04-12 17:26:31 +0800

    update:标题

--- src/components/layout/components/Sidebar/Logo.vue
+++ src/components/layout/components/Sidebar/Logo.vue
@@ -3,11 +3,11 @@
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <img v-if="sysInfo.logo" :src="sysInfo.logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title">{{ sysInfo.name }} </h1>
+        <h1 v-else class="sidebar-title">{{ $sysMainFrameInfo.APP_NAME }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="sysInfo.logo" :src="sysInfo.logo" class="sidebar-logo">
-        <h1 class="sidebar-title">{{ sysInfo.name }} </h1>
+        <h1 class="sidebar-title">{{ $sysMainFrameInfo.APP_NAME }} </h1>
       </router-link>
     </transition>
   </div>
@@ -30,8 +30,8 @@ export default {
   data() {
     return {
       sysInfo: {
-        name: '有道运维周报',
-        logo: require('@/assets/matrix-logo.png'),
+        name: '子系统模板',
+        logo: require('@/assets/logo.svg'),
       }
     }
   }
